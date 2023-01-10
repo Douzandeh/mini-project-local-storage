53c6d4cddc4ee4104b7900ce2471e2107cf7e0cd
Hossein <hosseindozandeh799@gmail.com>
2023-01-10 08:26:48 +0330

    getItem Local Storage

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-localStorage.setItem("firstName", "Hossein");
-localStorage.setItem("lastName" , "Douzandeh")
-localStorage.setItem("age" , 22)
-localStorage.setItem("access" , true)
+// localStorage.setItem("firstName", "Hossein");
+// localStorage.setItem("lastName" , "Douzandeh")
+// localStorage.setItem("age" , 22)
+// localStorage.setItem("access" , true)
+let firstName = localStorage.getItem("firstName")
+let lastName = localStorage.getItem("lastName")
+
+console.log(firstName , lastName);
