ddcd4b0785b6d9d3085d05a5c6540cac2b5cdc5d
Hossein <hosseindozandeh799@gmail.com>
2023-01-10 08:31:20 +0330

    Method ParseInt

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 // localStorage.setItem("lastName" , "Douzandeh")
 // localStorage.setItem("age" , 22)
 // localStorage.setItem("access" , true)
-let firstName = localStorage.getItem("firstName")
-let lastName = localStorage.getItem("lastName")
-
-console.log(firstName , lastName);
+// let firstName = localStorage.getItem("firstName")
+// let lastName = localStorage.getItem("lastName")
+let age = localStorage.getItem("age")
+age = parseInt(age)
+console.log(age + 1);
