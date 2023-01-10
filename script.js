998e6c715f0e4a4e7fa1be8ae188c855bd3359e9
Hossein <hosseindozandeh799@gmail.com>
2023-01-10 08:46:58 +0330

    Clear local storage

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
+// ? Initialization in local storage
 // localStorage.setItem("firstName", "Hossein");
 // localStorage.setItem("lastName" , "Douzandeh")
 // localStorage.setItem("age" , 22)
 // localStorage.setItem("access" , true)
+// ? Initialization in local storage
+
+
+// ? getItem Local Storage
 // let firstName = localStorage.getItem("firstName")
 // let lastName = localStorage.getItem("lastName")
+// ? getItem Local Storage
+
+
+// ? Method ParseInt
 // let age = localStorage.getItem("age")
 // age = parseInt(age)
-let access = localStorage.getItem("access")
-if(access === "true"){
-    console.log("You have access");
-}else{
-    console.log("You have not access");
-}
+// ? Method ParseInt
+
+
+// ? Boolean value in local storage
+// let access = localStorage.getItem("access")
+// if(access === "true"){
+//     console.log("You have access");
+// }else{
+//     console.log("You have not access");
+// }
+
+
+// ? Clear local storage
+localStorage.removeItem("firstName")
+localStorage.clear()
